d7fba26722ee591a72a378093ede32eeaed3c1b0
kawkoi <haruubusinesss@gmail.com>
2025-11-08 23:26:37 +0530

    box

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use ratatui::{
+    Terminal,
+    backend::CrosstermBackend,
+    widgets::{Block, Borders, Paragraph},
+};
+use std::io;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    todo!();
 }
+
