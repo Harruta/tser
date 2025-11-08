8fd1557fdae21a3ed4b7446e419b35492af073de
kawkoi <haruubusinesss@gmail.com>
2025-11-09 00:05:23 +0530

    basic TUI

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,39 @@ use crossterm::{
 };
 use ratatui::{
     Terminal,
-    backend::CrosstermBackend,
+    backend::{self, CrosstermBackend},
     widgets::{Block, Borders, Paragraph},
 };
 use std::io;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    todo!();
-}
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
+    terminal.draw(|f| {
+        let block = Block::default()
+            .title("Typing Speed Evaluator")
+            .borders(Borders::ALL);
+        f.render_widget(Paragraph::new("Press esc to quit").block(block), f.area());
+    })?;
+
+    loop {
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Esc {
+                break;
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
+}
